5d427cf252d58d73d569fda6f6ddbe1070c5bc51
Alfonso <afoone@hotmail.com>
2020-02-17 09:38:53 +0100

    inicial

---
+++ src/test/java/com/once/springservices/SpringservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.once.springservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
